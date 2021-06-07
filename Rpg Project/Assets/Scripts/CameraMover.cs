1d460f82cedef34eb19a8c95dfbfd9ffdcd442f5
vedant <vedantk699@gamil.com>
2021-06-07 10:20:31 -0700

    spawn level complete
    this is the level where we are going to spawn on and its quite fantastic to say ,
    i never thought it was gona be so good ,thanks to the tile set
    tommorow do new level  1 design f

---
+++ Rpg Project/Assets/Scripts/CameraMover.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    public Transform player_Position;
+    //what the difference should be between player and camers center before moving the camera
+    public float boundX = 0.15f;
+    public float boundY = 0.05f;
+
+
+    /// <summary>
+    /// we are using late update because we want the camera to move after the player have been moved
+    /// </summary>
+    private void LateUpdate()
+    {
+        Vector3 delta = Vector3.zero;
+
+        //we check by how much has the player moved wrt to the center of camera
+        float deltaX = player_Position.position.x - transform.position.x;
+
+        //if it is more than bounds that we have set in the inspector we move
+        if (deltaX > boundX || deltaX < -boundX)
+        {
+            if(transform.position.x < player_Position.position.x)
+            {
+                delta.x = deltaX - boundX;
+            }
+            else
+            {
+                delta.x = deltaX + boundX;
+            }
+        }
+
+        float deltaY = player_Position.position.y - transform.position.y;
+        if (deltaY > boundY || deltaY < -boundY)
+        {
+            if (transform.position.y < player_Position.position.y)
+            {
+                delta.y = deltaY - boundY;
+            }
+            else
+            {
+                delta.y = deltaY + boundY;
+            }
+        }
+
+        transform.position += new Vector3(delta.x, delta.y, 0f);
+
+
+
+    }
+
+}
--- Rpg Project/Assets/Scripts/PlayerMovement.cs
+++ Rpg Project/Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     #region Variables Declarations
     //requirements
     private BoxCollider2D _boxCollider;
+    RaycastHit2D hit;
 
     //inputs
     Vector2 moveDir;
@@ -44,6 +45,10 @@ public class PlayerMovement : MonoBehaviour
         moveDir = new Vector3(x, y);
     }
 
+
+    /// <summary>
+    /// use to flip the sprite in right left direction
+    /// </summary>
     private void SpriteFlipper()
     {
         if(moveDir.x >0)
@@ -58,7 +63,31 @@ public class PlayerMovement : MonoBehaviour
     private void Movement()
     {
         moveDir.Normalize();//to counter diagonal movement
-        transform.Translate(moveDir * Time.deltaTime*speed);
+
+        ///we check weather we can move movedir.y*time.delta timnes distance at that particualr frame when we are pressing the ip
+        ///1st argumnent-position to cast from,2nd arg - size of the collider,3rd arg - Angle
+        ///4th arg Max distance to cast ,5th layermask which we want to know if the player collided with
+        
+        //this is for y direction
+        hit = Physics2D.BoxCast(transform.position, _boxCollider.size, 0, new Vector2(0, moveDir.y), 
+            Mathf.Abs(moveDir.y * Time.deltaTime), LayerMask.GetMask("Actors", "Blocking"));
+        if(hit.collider == null)
+        {
+            transform.Translate(0f, moveDir.y * Time.deltaTime*speed, 0f);
+        }
+        //this is for x direction
+        hit = Physics2D.BoxCast(transform.position, _boxCollider.size, 0, new Vector2( moveDir.x,0f),
+            Mathf.Abs(moveDir.x * Time.deltaTime), LayerMask.GetMask("Actors", "Blocking"));
+        if (hit.collider == null)
+        {
+            transform.Translate( moveDir.x * Time.deltaTime * speed, 0f,0f);
+        }
+
+        //now this will initially will not move the player as it is colliding with itself as we are casting the box from player position and 
+        //player is also on the same layer
+        //so to avoid that go to physics and uncheck query start in collider,i.e it will ignore itself
+
+
     }
 
 }
